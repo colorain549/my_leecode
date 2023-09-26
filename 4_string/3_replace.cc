cf805e7f6320276f13d056dab044a83d22c6d7ed
lam <colorain@live.com>
2023-09-26 13:54:48 +0800

    modified 3_replace.cc

--- 4_string/3_replace.cc
+++ 4_string/3_replace.cc
@@ -8,6 +8,41 @@ using std::cout;
 using std::endl;
 using std::string;
 
+// 使用双指针
+class Solution
+{
+public:
+    string pathEncryption(string path)
+    {
+        // 原字符串长度
+        int len = path.size();
+
+        // 新字符串长度
+        string newPath;
+        newPath.resize(len);
+
+        // 原字符串最大下标
+        int i = len - 1;
+
+        // 新字符串最大下标
+        int j = len - 1;
+
+        while (i >= 0)
+        {
+            if (path[i] == '.')
+            {
+                newPath[j--] = ' ';
+                i--;
+            }
+            else
+            {
+                newPath[j--] = path[i--];
+            }
+        }
+        return newPath;
+    }
+};
+
 // 使用迭代器遍历
 // class Solution
 // {
@@ -26,22 +61,22 @@ using std::string;
 // };
 
 // 使用下标访问
-class Solution
-{
-public:
-    string pathEncryption(string path)
-    {
-        int len = path.size();
-        for (int i = 0; i < len; i++)
-        {
-            if (path[i] == '.')
-            {
-                path[i] = ' ';
-            }
-        }
-        return path;
-    }
-};
+// class Solution
+// {
+// public:
+//     string pathEncryption(string path)
+//     {
+//         int len = path.size();
+//         for (int i = 0; i < len; i++)
+//         {
+//             if (path[i] == '.')
+//             {
+//                 path[i] = ' ';
+//             }
+//         }
+//         return path;
+//     }
+// };
 
 int main()
 {
